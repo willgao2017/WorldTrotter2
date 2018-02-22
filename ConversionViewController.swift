5e78644c2510055c58fbaa76459d45208ce3e3a5
will <will.gao2017@outlook.com>
2018-02-22 09:21:34 +0800

    Initial Commit

---
+++ ConversionViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter2
+//
+//  Created by Will GAO on 07/02/2018.
+//  Copyright © 2018 Big Nerd Ranch. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>?{
+        didSet {
+            updateCelsiusLabel()
+            print("fuck1")
+
+        }
+    }
+    
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        //celsiusLabel.text = textField.text
+        /*if let text = textField.text, !text.isEmpty {
+         celsiusLabel.text = text
+         } else {
+         celsiusLabel.text = "???"
+         }*/
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            //celsiusLabel.text = "\(celsiusValue.value)"
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+            print("fuck2")
+
+        }
+    }
+    
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            print("ConversionViewController loaded its view.")
+        updateCelsiusLabel()
+
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true }
+    }
+    
+}
---
+++ WorldTrotter2Tests/WorldTrotter2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  WorldTrotter2Tests.swift
+//  WorldTrotter2Tests
+//
+//  Created by Will GAO on 06/02/2018.
+//  Copyright © 2018 Big Nerd Ranch. All rights reserved.
+//
+
+import XCTest
+@testable import WorldTrotter2
+
+class WorldTrotter2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
