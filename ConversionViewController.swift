0f358af8af5d48ebfa637acdced5cd1a2a5eff14
will <will.gao2017@outlook.com>
2018-02-22 09:29:07 +0800

    testing space

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -71,7 +71,7 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
             print("ConversionViewController loaded its view.")
-        updateCelsiusLabel()
+        updateCelsiusLabel() 
 
     }
     
